e43351b6b953f97464c8e3f868fa809d8fb10a99
sosnovskyas <sosnovsky.as@gmail.com>
2018-11-09 04:05:11 +0300

    implement pins control

--- src/main.cpp
+++ src/main.cpp
@@ -1,98 +1,147 @@
-/*
-   Copyright (c) 2015, Majenko Technologies
-   All rights reserved.
-
-   Redistribution and use in source and binary forms, with or without modification,
-   are permitted provided that the following conditions are met:
-
- * * Redistributions of source code must retain the above copyright notice, this
-     list of conditions and the following disclaimer.
-
- * * Redistributions in binary form must reproduce the above copyright notice, this
-     list of conditions and the following disclaimer in the documentation and/or
-     other materials provided with the distribution.
-
- * * Neither the name of Majenko Technologies nor the names of its
-     contributors may be used to endorse or promote products derived from
-     this software without specific prior written permission.
-
-   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-   ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-   WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-   DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
-   ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-   (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
-   ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
-#include <ESP8266mDNS.h>
+
+#define HANDSET_PIN 16  // D0
+#define DOOR_PIN 5      // D1
+#define RING_PIN 4     // D2
 
 const char *ssid = "dd-wrt";
 const char *password = "z8X26q19";
 
 ESP8266WebServer server(80);
 
-const int led = 13;
-
 void handleRoot() {
-    digitalWrite(led, 1);
-    char temp[400];
-    int sec = millis() / 1000;
-    int min = sec / 60;
-    int hr = min / 60;
+    char temp[1400];
 
-    snprintf(temp, 400,
-
-             "<html>\
-  <head>\
-    <meta http-equiv='refresh' content='5'/>\
-    <title>ESP8266 Demo</title>\
-    <style>\
-      body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }\
-    </style>\
-  </head>\
-  <body>\
-    <h1>Hello from ESP8266!</h1>\
-    <p>Uptime: %02d:%02d:%02d</p>\
-    <img src=\"/test.svg\" />\
-  </body>\
-</html>",
-
-             hr, min % 60, sec % 60
-    );
+    snprintf(temp, 1400, "<!DOCTYPE html>\n"
+                         "<html lang=\"en\">\n"
+                         "<head>\n"
+                         "    <meta charset=\"UTF-8\">\n"
+                         "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n"
+                         "    <title>Doorman</title>\n"
+                         "    <style>\n"
+                         "        body {\n"
+                         "            display: flex;\n"
+                         "\n"
+                         "            flex-direction: column;\n"
+                         "            align-items: center;\n"
+                         "        }\n"
+                         "\n"
+                         "        .buttons {\n"
+                         "            display: flex;\n"
+                         "\n"
+                         "            background-color: darkgray;\n"
+                         "\n"
+                         "            justify-content: space-between;\n"
+                         "            align-items: center;\n"
+                         "\n"
+                         "            width: 150px;\n"
+                         "            padding: 20px;\n"
+                         "        }\n"
+                         "\n"
+                         "        button {\n"
+                         "            background-color: cadetblue;\n"
+                         "            height: 30px;\n"
+                         "            width: 60px;\n"
+                         "        }\n"
+                         "    </style>\n"
+                         "</head>\n"
+                         "<body>\n"
+                         "<button class=\"open-door-button\" style=\"margin-bottom: 80px\">open door</button>\n"
+                         "\n"
+                         "\n"
+                         "<div class=\"buttons\">\n"
+                         "    <button onclick=\"fetch('/up')\">up</button>\n"
+                         "    <button onclick=\"fetch('/down')\">down</button>\n"
+                         "</div>\n"
+                         "\n"
+                         "<div class=\"buttons\">\n"
+                         "    <button onclick=\"fetch('/open')\">open</button>\n"
+                         "    <button onclick=\"fetch('/close')\">close</button>\n"
+                         "</div>\n"
+                         "\n"
+                         "<script>\n"
+                         "    document\n"
+                         "        .querySelector('.open-door-button')\n"
+                         "        .addEventListener('click', (event) => {\n"
+                         "            const target = event.target;\n"
+                         "\n"
+                         "            target.setAttribute('disabled', true);\n"
+                         "            setTimeout(() => {target.removeAttribute('disabled')}, 6000);\n"
+                         "\n"
+                         "            fetch('/open-door');\n"
+                         "        })\n"
+                         "</script>\n"
+                         "\n"
+                         "</body>\n"
+                         "</html>");
     server.send(200, "text/html", temp);
-    digitalWrite(led, 0);
 }
 
 void handleNotFound() {
-    digitalWrite(led, 1);
-    String message = "File Not Found\n\n";
-    message += "URI: ";
-    message += server.uri();
-    message += "\nMethod: ";
-    message += (server.method() == HTTP_GET) ? "GET" : "POST";
-    message += "\nArguments: ";
-    message += server.args();
-    message += "\n";
-
-    for (uint8_t i = 0; i < server.args(); i++) {
-        message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-    }
-
-    server.send(404, "text/plain", message);
-    digitalWrite(led, 0);
+    server.send(404);
 }
 
+String getStatus() {
+    return "{\
+           HANDSET:" + String(digitalRead(HANDSET_PIN)) + ",\
+           DOOR:" + String(digitalRead(DOOR_PIN)) + "\
+                                                        }";
+}
+
+void up() {
+    digitalWrite(HANDSET_PIN, HIGH);
+    Serial.println("UP");
+    server.send(200, "application/json", getStatus());
+}
+
+void down() {
+    digitalWrite(HANDSET_PIN, LOW);
+    Serial.println("DOWN");
+    server.send(200, "application/json", getStatus());
+}
+
+void open() {
+    digitalWrite(DOOR_PIN, HIGH);
+    Serial.println("UP");
+    server.send(200, "application/json", getStatus());
+}
+
+void close() {
+    digitalWrite(DOOR_PIN, LOW);
+    Serial.println("DOWN");
+    server.send(200, "application/json", getStatus());
+}
+
+void status() {
+    server.send(200, "application/json", getStatus());
+}
+
+void openDoor() {
+    digitalWrite(HANDSET_PIN, HIGH);
+    delay(1000);
+    digitalWrite(DOOR_PIN, HIGH);
+    delay(1000);
+    digitalWrite(DOOR_PIN, LOW);
+    delay(1000);
+    digitalWrite(HANDSET_PIN, LOW);
+    delay(1000);
+
+    Serial.println("OPEN DOOR");
+    server.send(200, "application/json", getStatus());
+}
+
+
 void setup(void) {
-    pinMode(led, OUTPUT);
-    digitalWrite(led, 0);
+    pinMode(HANDSET_PIN, OUTPUT);
+    pinMode(DOOR_PIN, OUTPUT);
+    pinMode(RING_PIN, INPUT_PULLUP);
+
+    digitalWrite(HANDSET_PIN, LOW);
+    digitalWrite(DOOR_PIN, LOW);
+
     Serial.begin(115200);
     WiFi.mode(WIFI_STA);
     WiFi.begin(ssid, password);
@@ -104,20 +153,16 @@ void setup(void) {
         Serial.print(".");
     }
 
-    Serial.println("");
-    Serial.print("Connected to ");
-    Serial.println(ssid);
     Serial.print("IP address: ");
     Serial.println(WiFi.localIP());
 
-    if (MDNS.begin("esp8266")) {
-        Serial.println("MDNS responder started");
-    }
-
     server.on("/", handleRoot);
-    server.on("/inline", []() {
-        server.send(200, "text/plain", "this works as well");
-    });
+    server.on("/up", up);
+    server.on("/down", down);
+    server.on("/open", open);
+    server.on("/close", close);
+    server.on("/status", status);
+    server.on("/open-door", openDoor);
     server.onNotFound(handleNotFound);
     server.begin();
     Serial.println("HTTP server started");
